477ca16c3edf82c330e52c5d72f786aef9d6e11f
Silas dos Santos Caimi <silas.caimi@live.com>
2023-05-11 12:38:15 -0300

    Configurando o Refresh Token Grant Type no Authorization Server

--- src/main/java/br/com/silascaimi/auth/AuthorizationServerConfig.java
+++ src/main/java/br/com/silascaimi/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.silascaimi.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,13 +21,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	// Configurando os clientes que podem acessar o authorization em memória
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("esr-web")
 				.secret(passwordEncode.encode("web123"))
-				.authorizedGrantTypes("password")
+				.authorizedGrantTypes("password", "refresh_token")
 				.scopes("write", "read")
 				.accessTokenValiditySeconds(60 * 60 * 6)
 			.and()
@@ -45,6 +49,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService); // especifica o userDetailsService para trabalhar com refreshtoken
 	}
 }
