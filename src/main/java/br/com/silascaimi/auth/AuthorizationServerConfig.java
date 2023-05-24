3b67d47a93e88506429e359f7b99bed4e61bed22
Silas dos Santos Caimi <silas.caimi@live.com>
2023-05-23 21:47:26 -0300

    reativando o autorização com granularidade de escopo para capturar o code após sair depois de configurar o jwt

--- src/main/java/br/com/silascaimi/auth/AuthorizationServerConfig.java
+++ src/main/java/br/com/silascaimi/auth/AuthorizationServerConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
@@ -89,7 +92,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.reuseRefreshTokens(false)
 			//.tokenStore(redisTokenStore())
 			.accessTokenConverter(jwtAccessTokerConverter())
+			.approvalStore(approvalTokenStore(endpoints.getTokenStore()))
 			.tokenGranter(tokenGranter(endpoints));
+	}
+	
+	private ApprovalStore approvalTokenStore(TokenStore tokenStore) {
+		var approvalStore = new TokenApprovalStore();
+		approvalStore.setTokenStore(tokenStore);
+		return approvalStore;
 	}
 	
 	// remove configurações do redis para inclusão de jwt
